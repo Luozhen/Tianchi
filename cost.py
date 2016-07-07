b72c5cffe4f1b3b83e1355764063caf10f392513
Luozhen <luozhen523cherish@outlook.com>
2016-07-07 16:27:08 +0800

    cost in original

---
+++ cost.py
@@ -0,0 +1,16 @@
+#! user/bin/env python
+# -*- coding:utf-8 -*-
+# import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv(r'./resource/result.csv',
+                    names = ['Counter_id', 'Addr', 'Arrival_time', 'Departure', 'Amount', 'Order_id'])   # 读取表格
+
+counters = set(data['Counter_id'])
+totalTime = 0
+for pers in counters:
+    minArrive = min(data['Arrival_time'][data['Counter_id'] == pers])
+    maxDeparture = max(data['Departure'][data['Counter_id'] == pers])
+    totalTime += (maxDeparture - minArrive)
+print totalTime
